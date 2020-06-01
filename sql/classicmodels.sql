734da043c1b586b9c467363759fdf992197397f3
Phi-Doan VU <vuphidoan@gmail.com>
2020-06-01 15:48:44 +0200

    Update classicmodels.sql

--- sql/classicmodels.sql
+++ sql/classicmodels.sql
@@ -16,6 +16,7 @@ SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `classicmodels` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `classicmodels`;
 -- --------------------------------------------------------
+--test
 
 --
 -- Structure de la table `customers`
