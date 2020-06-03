b4f573c148e362e7ee890fb6fb4e35ba727635f4
Phi-Doan VU <vuphidoan@gmail.com>
2020-06-03 10:22:19 +0200

    retirer l'aide au debug de create_customer

--- src/create_customer.php
+++ src/create_customer.php
@@ -4,7 +4,8 @@ $pdo = new PDO('mysql:host=localhost;dbname=classicmodels', 'root', 'root');
 
 // Paramétrage de la liaison PHP <-> MySQL, les données sont encodées en UTF-8.
 $pdo->exec('SET NAMES UTF8');
-print_r("debug " . $_POST) ;
+    //HELP TO DEBUG
+        //print_r("debug " . $_POST) ;
 if(empty($_POST))
     {
         // Récupération de tous les clients du site.
@@ -34,20 +35,21 @@ if(empty($_POST))
                  (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
          ';
          $resultSet3=$pdo->prepare($query3);
-         print_r($_POST['customerNumber']." ");
-         print_r($_POST['customerName']." ");
-         print_r($_POST['contactLastName']." ");
-         print_r($_POST['contactFirstName']." ");
-         print_r($_POST['phone']." ");
-         print_r($_POST['addressLine1']." ");
-         print_r($_POST['addressLine2']." ");
-         print_r($_POST['city']." ");
-         print_r($_POST['state']." ");
-         print_r($_POST['postalCode']." ");
-         print_r($_POST['country']." ");
-         print_r($_POST['salesRepEmployeeNumber']." ");
-         print_r($_POST['creditLimit']." ");
-         print_r($_POST) ;
+            //HELP TO DEBUG
+                //print_r($_POST['customerNumber']." ");
+                //print_r($_POST['customerName']." ");
+                //print_r($_POST['contactLastName']." ");
+                //print_r($_POST['contactFirstName']." ");
+                //print_r($_POST['phone']." ");
+                //print_r($_POST['addressLine1']." ");
+                //print_r($_POST['addressLine2']." ");
+                //print_r($_POST['city']." ");
+                //print_r($_POST['state']." ");
+                //print_r($_POST['postalCode']." ");
+                //print_r($_POST['country']." ");
+                //print_r($_POST['salesRepEmployeeNumber']." ");
+                //print_r($_POST['creditLimit']." ");
+                //print_r($_POST) ;
          $resultSet3->execute([$_POST['customerNumber'], $_POST['customerName'], $_POST['contactLastName'], $_POST['contactFirstName'], $_POST['phone'], $_POST['addressLine1'], $_POST['addressLine2'], $_POST['city'], $_POST['state'], $_POST['postalCode'], $_POST['country'], $_POST['salesRepEmployeeNumber'], $_POST['creditLimit']]);
          //$resultSet3->execute(array('10427', 'commentaire', '456'));
  
