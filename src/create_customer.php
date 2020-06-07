a48a1a3b5c7cd4fdfd45d7ac517a942453300bb6
Phi-Doan VU <vuphidoan@gmail.com>
2020-06-07 23:17:37 +0200

    Update create_customer.php

--- src/create_customer.php
+++ src/create_customer.php
@@ -1,9 +1,5 @@
 <?php
-
-$pdo = new PDO('mysql:host=localhost;dbname=classicmodels', 'root', 'root');
-
-// Paramétrage de la liaison PHP <-> MySQL, les données sont encodées en UTF-8.
-$pdo->exec('SET NAMES UTF8');
+include 'application/bdd_connection.php';
     //HELP TO DEBUG
         //print_r("debug " . $_POST) ;
 if(empty($_POST))
