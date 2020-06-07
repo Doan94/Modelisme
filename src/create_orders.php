26738d3a0066ce9ffdb478645813c61915da95b3
Phi-Doan VU <vuphidoan@gmail.com>
2020-06-07 23:17:40 +0200

    Update create_orders.php

--- src/create_orders.php
+++ src/create_orders.php
@@ -1,9 +1,5 @@
 <?php
-
-$pdo = new PDO('mysql:host=localhost;dbname=classicmodels', 'root', 'root');
-
-// Paramétrage de la liaison PHP <-> MySQL, les données sont encodées en UTF-8.
-$pdo->exec('SET NAMES UTF8');
+include 'application/bdd_connection.php';
 
 if(empty($_POST))
     {
