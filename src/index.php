32578b64f918dd05e1c50dade09845d9ac96ca92
Phi-Doan VU <vuphidoan@gmail.com>
2020-06-07 23:17:43 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,18 +1,5 @@
 <?php
-
-/*
- * Construction d'un objet PDO représentant la connexion à la base de données.
- *
- * Le premier argument est un Data Source Name (DSN) représentant où est-ce qu'il
- * faut se connecter. Une adresse IP ou un nom de domaine peut être spécifié.
- *
- * /!\ Tout le DSN doit être écrit en minuscules et sans espaces.
- *
- */
-$pdo = new PDO('mysql:host=localhost;dbname=classicmodels', 'root', 'root');
-
-// Paramétrage de la liaison PHP <-> MySQL, les données sont encodées en UTF-8.
-$pdo->exec('SET NAMES UTF8');
+include 'application/bdd_connection.php';
 
 
 
